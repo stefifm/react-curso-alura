b764635c336b44442aac88d2bf31166b1c58f4e3
stefifm <bruerastefania@gmail.com>
2023-04-05 11:46:16 -0300

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import Header from './components/Header/Header'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
+      <Header />
       <div>
-        <img
-          src='/img/header.png'
-          alt='Header de Org'
-        />
         <a
           href='https://reactjs.org'
-          target='_blank'>
+          target='_blank'
+          rel='noreferrer'>
           <img
             src={reactLogo}
             className='logo react'
@@ -24,7 +19,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>HOLA MUNDO CON REACT</p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div>
+      <img
+        src='/img/header.png'
+        alt='Header de Org'
+      />
+    </div>
+  )
+}
