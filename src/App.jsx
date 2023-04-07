21c999e01fa28c4d426738e849a811a7a5b4b9d9
stefifm <bruerastefania@gmail.com>
2023-04-06 22:02:04 -0300

    Add Formulario component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Formulario from './components/Formulario/Formulario'
 import Header from './components/Header/Header'
 function App() {
   return (
     <div>
       <Header />
+      <Formulario />
     </div>
   )
 }
