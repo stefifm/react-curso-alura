95d9e16fa10590224db53c2d8a8a359de6920c5d
stefifm <bruerastefania@gmail.com>
2023-04-11 23:22:45 -0300

    Change css style and insert rename Campo

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
 import Boton from '../Boton'
-import CampoTexto from '../CampoTexto'
 import ListaOpciones from '../ListaOpciones'
 
 import './Formulario.css'
+import Campo from '../Campo'
 
 function Formulario({ equipos, registrarColaborador, crearEquipo }) {
   const [nombre, setNombre] = useState('')
@@ -41,21 +41,21 @@ function Formulario({ equipos, registrarColaborador, crearEquipo }) {
         action=''
         onSubmit={manejarEnvio}>
         <h2>Rellena el formulario para crear el colaborador.</h2>
-        <CampoTexto
+        <Campo
           title='Nombre'
           placeholder='Ingresar Nombre'
           required
           value={nombre}
           setValue={setNombre}
         />
-        <CampoTexto
+        <Campo
           title='Puesto'
           placeholder='Ingresar Puesto'
           required
           value={puesto}
           setValue={setPuesto}
         />
-        <CampoTexto
+        <Campo
           title='Foto'
           placeholder='Ingresar enlace de la Foto'
           required
@@ -76,19 +76,20 @@ function Formulario({ equipos, registrarColaborador, crearEquipo }) {
         action=''
         onSubmit={handleSubmitTeam}>
         <h2>Rellena el formulario para crear el equipo.</h2>
-        <CampoTexto
+        <Campo
           title='Título'
           placeholder='Ingresar Titulo'
           required
           value={titulo}
           setValue={setTitulo}
         />
-        <CampoTexto
+        <Campo
           title='Color'
           placeholder='Ingresar el color en hexadecimal'
           required
           value={color}
           setValue={setColor}
+          type='color'
         />
         <Boton>Registrar Equipo</Boton>
       </form>
